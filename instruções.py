ae986d5b3b28086315e0dc7664b4bdf180bd5373
Luannarabelo <74067874+Luannarabelo@users.noreply.github.com>
2022-08-01 17:19:19 -0300

    Add files via upload

---
+++ instruções.py
@@ -0,0 +1,137 @@
+fazer uma função para cada intrução
+trocando o 'ADC' por cada uma das instruções 
+e trocando o '# aaa' pelo devido endereçamento
+
+    def ADC(self,ins):
+        # aaa
+
+    def AND(self,ins):
+        # aaa
+    
+    def ASL(self,ins):
+        # bb
+
+    def BCC(self,ins): 
+
+    def BCS(self,ins):
+
+    def BEQ(self,ins):
+
+    def BIT(self,ins):
+        # x
+
+    def BMI(self,ins):
+        
+    def BNE(self,ins):
+
+    def BPL(self,ins):
+    
+    def BRK(self,ins):
+    
+    def BVC(self,ins):
+    
+    def BVS(self,ins):
+    
+    def CLC(self,ins):
+    
+    def CLD(self,ins):
+    
+    def CLI(self,ins):
+    
+    def CLV(self,ins):
+    
+    def CMP(self,ins):
+        # aaa
+
+    def CPX(self,ins):
+        # cc
+
+    def CPY(self,ins):
+        # cc
+
+    def DEC(self,ins):
+        # bb
+
+    def DEX(self,ins):
+        
+    def DEY(self,ins):
+        
+    def EOR(self,ins):
+        # aaa
+
+    def INC(self,ins):
+        # bb
+
+    def INX(self,ins):
+    
+    def INY(self,ins):
+    
+    def JPM(self,ins):
+        # y
+
+    def JST(self,ins):
+    
+    def LDA(self,ins):
+        # aaa 
+
+    def LDX(self,ins):
+        # ddd
+
+    def LDY(self,ins):
+        # ddd
+
+    def LSR(self,ins):
+        # bb
+
+    def NOP(self,ins):
+        
+    def ORA(self,ins):
+        # aaa
+
+    def PHA(self,ins):
+        
+    def PHP(self,ins):
+    
+    def PLA(self,ins):
+    
+    def PLP(self,ins):
+    
+    def ROL(self,ins):
+        # bb
+
+    def ROR(self,ins):
+        # bb
+
+    def RTI(self,ins):
+    
+    def RTS(self,ins):
+    
+    def SBC(self,ins):
+        # aaa
+
+    def SEC(self,ins):
+
+    def SED(self,ins):
+        
+    def SEI(self,ins):
+    
+    def STA(self,ins):
+        # aaa
+
+    def STX(self,ins):
+        # bb
+
+    def STY(self,ins):
+        # bb
+        
+    def TAX(self,ins):
+
+    def TAY(self,ins):
+    
+    def TSX(self,ins):
+    
+    def TXA(self,ins):
+    
+    def TXS(self,ins):
+    
+    def TYA(self,ins):
